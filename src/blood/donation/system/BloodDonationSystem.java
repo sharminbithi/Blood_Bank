598c4cfa368388cf3c1d365b7892d9685e413c1d
sharminbithi <jannatbithi39@gmail.com>
2021-02-18 00:25:52 +0600

    My SDP3 Project

---
+++ src/blood/donation/system/BloodDonationSystem.java
@@ -0,0 +1,13 @@
+
+package blood.donation.system;
+
+
+public class BloodDonationSystem {
+
+    
+    public static void main(String[] args) {
+       startpage  obj= new startpage();
+       obj.show();
+    }
+    
+}
